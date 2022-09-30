590cff0d725dcb241531662542f1da617620f15c
Maksym Havrylov <salomondr007@gmail.com>
2022-09-30 22:49:04 +0300

    dropdown menu, crear mainData.js, -main.js

---
+++ js/categoriesData.js
@@ -0,0 +1,5 @@
+const categoriesData = () => {
+
+};
+
+categoriesData();
--- js/mainData.js
+++ js/mainData.js
@@ -1,9 +1,17 @@
 const mainData = () => {
 
+  const renderGanreList = (ganres) => {
+    const dropdownBlock = document.querySelector('.header__menu .dropdown');
+
+    ganres.forEach(ganre => {
+      dropdownBlock.insertAdjacentHTML('beforeend', `
+        <li><a href="./categories.html&ganre=${ganre}">${ganre}</a></li>
+      `)
+    });
+  };
+
   const renderAnimeList = (array, ganres) => {
     const wrapper = document.querySelector('.product .col-lg-8');
-
-    wrapper.innerHTML = '';
 
     ganres.forEach((ganre) => {
       const productBlock = document.createElement('div');
@@ -13,7 +21,7 @@ const mainData = () => {
       listBlock.classList.add('row');
       productBlock.classList.add('mb-5');
 
-      productBlock.insertAdjacentHTML('afterbegin', `
+      productBlock.insertAdjacentHTML('beforeend', `
         <div class="row">
           <div class="col-lg-8 col-md-8 col-sm-8">
             <div class="section-title">
@@ -22,8 +30,7 @@ const mainData = () => {
           </div>
           <div class="col-lg-4 col-md-4 col-sm-4">
             <div class="btn__all">
-              <a href="/categories.html" class="primary-btn">View All <span
-                class="arrow_right"></span></a>
+              <a href="/categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
             </div>
           </div>
         </div>
@@ -33,14 +40,12 @@ const mainData = () => {
         const tagsBlock = document.createElement('ul');
 
         item.tags.forEach(tag => {
-          tagsBlock.insertAdjacentHTML('afterbegin', `
+          tagsBlock.insertAdjacentHTML('beforeend', `
             <li>${tag}</li>
-          `);
+          `)
         });
 
-        console.dir(tagsBlock);
-
-        listBlock.insertAdjacentHTML('afterbegin', `
+        listBlock.insertAdjacentHTML('beforeend', `
           <div class="col-lg-4 col-md-6 col-sm-6">
             <div class="product__item">
               <div class="product__item__pic set-bg" data-setbg="${item.image}">
@@ -49,12 +54,12 @@ const mainData = () => {
               </div>
               <div class="product__item__text">
                 ${tagsBlock.outerHTML}
-                <h5><a href="/anime-details.html">${item}</a>
+                <h5><a href="/anime-details.html?itemId=${item.id}">${item.title}</a>
                 </h5>
               </div>
             </div>
           </div>
-        `);
+        `)
       });
 
       productBlock.append(listBlock);
@@ -68,11 +73,9 @@ const mainData = () => {
 
   const renderTopAnime = (array) => {
     const wrapper = document.querySelector('.filter__gallery');
-
-    wrapper.innerHTML = '';
     
     array.forEach((item) => {
-      wrapper.insertAdjacentHTML('afterbegin', `
+      wrapper.insertAdjacentHTML('beforeend', `
         <div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
           <div class="ep">${item.rating} / 10</div>
           <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
@@ -84,22 +87,20 @@ const mainData = () => {
     wrapper.querySelectorAll('.set-bg').forEach((elem) => {
       elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
     });
-  }
+  };
 
-  fetch('./db.json')
-    .then((response) => {
-      return response.json();
-    })
+  fetch('../db.json')
+    .then((response) => response.json())
     .then((data) => {
       const ganres = new Set();
-
-      renderTopAnime(data.anime.sort((a, b) => b.views - a.views).slice(0, 5));
 
       data.anime.forEach((item) => {
         ganres.add(item.ganre);
       });
 
+      renderTopAnime(data.anime.sort((a, b) => b.views - a.views).slice(0, 5));
       renderAnimeList(data.anime, ganres);
+      renderGanreList(ganres);
     })
 };
   
